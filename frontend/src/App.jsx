111cecca8e8d57316668172fe0be62014f52af40
kevin joel noviello <kjnoviello@gmail.com>
2024-02-20 10:42:25 -0300

    fix

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css'
 import Footer from './assets/components/Footer/Footer'
 import Navbar from './assets/components/Navbar/Navbar'
+import Search from './assets/components/Search/Search'
 import Proceso from './assets/pages/Proceso/Proceso'
 
 
@@ -9,6 +10,7 @@ function App() {
   return (
     <div className="app">
       <Navbar />
+      <Search />
       <Proceso />
       <Footer />
     </div>
