ffa523c9b806fb84058a1d306aaadfa291330255
kevin joel noviello <kjnoviello@gmail.com>
2024-04-16 09:09:33 -0300

    add login

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,7 @@ import Navbar from './assets/components/Navbar/Navbar'
 import Nosotros from './assets/pages/Nosotros/Nosotros'
 import Proceso from './assets/pages/Proceso/Proceso'
 import Carousel from './assets/components/Carousel/Carousel'
+import Login from './assets/pages/Login/Login'
 
 function App() {
 
@@ -15,6 +16,7 @@ function App() {
       <Carousel/>
       <Nosotros />
       <Footer />
+      <Login />
     </div>
   )
 }
